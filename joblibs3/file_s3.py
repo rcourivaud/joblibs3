d0ff8c68e000ab77e5613988bf2cbb75b5ee0f7e
Raphaël Courivaud <raph@getkudoz.com>
2018-05-16 14:32:54 +0200

    add File S3 handling

---
+++ joblibs3/file_s3.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+"""Main module."""
+
+import s3io
+
+class FileS3(object):
+    def __init__(self, aws_key, aws_private_key, bucket=None,
+                 compression_type="gzip", compression_rate=3):
+        self.aws_key = aws_key
+        self.aws_private_key = aws_private_key
+        self.compression_type = compression_type
+        self.compression_rate = compression_rate
+        self.compress = (self.compression_type, self.compression_rate) \
+            if self.compression_type and self.compression_rate else None
+        self.bucket = bucket
+
+        self.credentials = dict(
+            aws_access_key_id=self.aws_key,
+            aws_secret_access_key=self.aws_private_key,
+        )
+
+    def save(self, obj, filename, bucket=None):
+        # Dump in an S3 file is easy with Joblib
+        bucket_to_save = bucket if bucket else self.bucket
+        if bucket_to_save is None:
+            raise AttributeError("Bucket must be specified")
+        with s3io.open('s3://{0}/{1}'.format(bucket, filename), mode='w',
+                       **self.credentials) as s3_file:
+            s3_file.write(obj)
+
+    def load(self, filename, bucket=None):
+        bucket_to_save = bucket if bucket else self.bucket
+        if bucket_to_save is None:
+            raise AttributeError("Bucket must be specified")
+
+        with s3io.open('s3://{0}/{1}'.format(bucket if bucket
+                                             else self.bucket,
+                                             filename), mode='r',
+                       **self.credentials) as s3_file:
+            obj = s3_file.read()
+
+        return obj
